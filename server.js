df617930f0cb10008e98512eb6c6c6950605a280
Jim Nelson <jim@jimnelson.us>
2022-06-06 23:24:45 -0700

    working POST to Mongo

--- server.js
+++ server.js
@@ -8,18 +8,23 @@ const PORT = process.env.PORT || 3000;
 MongoClient.connect(`mongodb+srv://pawn:${encodeURIComponent(process.env.MONGOPW)}@cluster0.b8f63.mongodb.net/?retryWrites=true&w=majority`, (err, client) => {
     console.log('Connected to Mongo Atlas');
     const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/index.html');
+    })
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/');
+            })
+            .catch(err => console.error(err));
+    });
+
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`);
+    })
 });
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
-
-app.post('/quotes', (req, res) => {
-    console.log(req.body);
-});
-
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
