664d1f8268c27f86646d99b4a8bc3839f463e1a3
Jim Nelson <jim@jimnelson.us>
2022-06-06 23:16:42 -0700

    connecting to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+MongoClient.connect(`mongodb+srv://pawn:${encodeURIComponent(process.env.MONGOPW)}@cluster0.b8f63.mongodb.net/?retryWrites=true&w=majority`, (err, client) => {
+    console.log('Connected to Mongo Atlas');
+    const db = client.db('star-wars-quotes');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
